4c9e67fe06992b9b0d4dc389f513601c6f26e485
gam0022 <gam0022@gmail.com>
2023-08-26 19:40:09 +0900

    Add copy-to-submit.bat

---
+++ copy-to-submit.bat
@@ -0,0 +1,14 @@
+set srcDir=D:\Dropbox\RTCamp\redflash_rtcamp9\build\
+set srcCudaDir=D:\Dropbox\RTCamp\redflash_rtcamp9\redflash\
+set dstDir=D:\Dropbox\RTCamp\rtcamp9_submit_gpu\
+
+@rem cp %srcDir%bin\Release\rtcamp8.bat %dstDir%
+cp %srcDir%bin\Release\redflash.exe %dstDir%
+cp %srcDir%bin\Release\sutil_sdk.dll %dstDir%
+cp %srcCudaDir%redflash.h %dstDir%cuda\
+cp %srcCudaDir%redflash.cu %dstDir%cuda\
+cp %srcCudaDir%bsdf_diffuse.cu %dstDir%cuda\
+cp %srcCudaDir%bsdf_disney.cu %dstDir%cuda\
+cp %srcCudaDir%intersect_raymarching.cu %dstDir%cuda\
+cp %srcCudaDir%intersect_sphere.cu %dstDir%cuda\
+cp %srcCudaDir%random.h %dstDir%cuda\
